38a825b2cacaa11f225928af1416c2a374e3c52c
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-08-28 15:30:21 +0200

    Job04 done

---
+++ Jour02/Job04/Pingouin.hpp
@@ -0,0 +1,41 @@
+#ifndef PINGOUIN_HPP
+#define PINGOUIN_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <memory>
+#include "Aquatique.hpp"
+#include "Terrestre.hpp"
+
+using namespace std;
+
+class Pingouin : public Aquatique, public Terrestre
+{
+    public:
+        Pingouin(string name, double speedEau, double speedTerre);
+        Pingouin(string name, double speedEau, double speedTerre, double speedGlisse);
+        ~Pingouin();
+        void saluer();
+        void nager() const override;
+        void marcher() const override;
+        void glisser() const;
+        void setVitesseGlisse(double speed);
+        double getVitesseGlisse() const;
+        void setVitesseEau(double speed);
+        double getVitesseEau() const;
+        void setVitesseTerre(double speed);
+        double getVitesseTerre() const;
+        static vector<shared_ptr<Pingouin>> PingouinS;
+        static void ajouterPingouin(shared_ptr<Pingouin> pingouin);
+        static void afficherPingouins();
+        static void supprimerPingouin(shared_ptr<Pingouin> pingouin);
+        double tempsParcours() const;
+        static void afficherTempsParcours();
+    private:
+        string nom;
+        double vitesseGlisse;
+};
+
+#endif // PINGOUIN_HPP
---
+++ Jour02/Job04/main.cpp
@@ -0,0 +1,34 @@
+#include "Pingouin.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Pingouin pingouin(10.0, 10.0, 15.0);
+    // pingouin.saluer();
+    // pingouin.nager();
+    // pingouin.marcher();
+    // pingouin.glisser();
+    // pingouin.setVitesseEau(20.0);
+    // pingouin.setVitesseTerre(20.0);
+    // pingouin.setVitesseGlisse(25.0);
+    // pingouin.nager();
+    // pingouin.marcher();
+    // pingouin.glisser();
+    shared_ptr<Pingouin> pingouin1 = make_shared<Pingouin>("Pierre", 10.0, 20.0, 30.0);
+    shared_ptr<Pingouin> pingouin2 = make_shared<Pingouin>("Paul", 30.0, 10.0, 20.0);
+    shared_ptr<Pingouin> pingouin3 = make_shared<Pingouin>("Jacques", 20.0, 30.0, 10.0);
+    Pingouin::ajouterPingouin(pingouin1);
+    Pingouin::ajouterPingouin(pingouin2);
+    Pingouin::ajouterPingouin(pingouin3);
+    // Pingouin::afficherPingouins();
+    // Pingouin::supprimerPingouin(pingouin2);
+    // Pingouin::afficherPingouins();
+    Pingouin::afficherTempsParcours();
+    cout << "test" << endl;
+    return 0;
+}
+// Il faut écrire :
+// cd Jour02/Job04
+// g++ main.cpp Pingouin.cpp Aquatique.cpp Terrestre.cpp -o main.exe
+// ./main
